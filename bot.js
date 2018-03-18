c1c80e97e8d79a3817c36ce0e0b8b3c3f97e346a
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-03-18 12:49:14 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,12 +13,40 @@ client.on('message', msg => {
 }});
 
 client.on('message', msg => {
+  if (msg.content === 'b!info') {
+    msg.reply({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "BlitzBot v0.0.2",
+    description: "Made by Sandal",
+    fields: [{
+        name: "Bot created: 3/17/18",
+        value: "Last update: 3/18/18"
+      },
+      {
+        name: "Made for Geometry Blitz",
+        value: "[Invite Here](https://discord.gg/XSKx36y)."
+      },
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Blitz-Bot"
+    }
+  }
+});
+
+client.on('message', msg => {
   if (msg.content === 'b!embed') {
     msg.reply({embed: {
   color: 3447003,
   description: "this is an embed!"
 }});
 
-      }
+  }
 });
+
 client.login(process.env.BOT_TOKEN);
