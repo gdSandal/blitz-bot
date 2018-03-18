de65cb8c76692120f0746479d817de2c80e4e1a6
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-03-18 00:07:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,12 @@ client.on('message', msg => {
 
 client.on('message', msg => {
   if (msg.content === 'b!info') {
-    msg.reply('Created by **Sandal**');
+    msg.reply({embed: {
+      color: 64fff7,
+      description:
+      "**BlitzBot v0.0.1**
+      made by: Sandal"
+    }});
   }
 });
 
