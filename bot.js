c2f00e56266992e3a12663b545a4a10d56c58141
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-03-18 00:55:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const db = require('quick.db');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -16,5 +17,26 @@ client.on('message', msg => {
     msg.reply('**BlitzBot // v0.0.1** -Made by Sandal-');
   }
 });
+
+db.updateValue(message.author.id + message.guild.id, 1). then(i => {
+  
+  let messages;
+  if (i.value == 25) messages = 25;
+  else if (i.value == 50) messages = 50;
+  else if (i.value == 100) messages = 100;
+  
+  if (!isNaN(messages)) {
+    db.updateValue('userLevel_${message.author.id + message.guild.id}',1).then(o => {
+      message.channel.send(' **Congradulations!** You reached ${messages} Blix, and have leveled up! you are now level ${o.value}')
+  }
+});
+
+exports.run - (bot, messages) -> {
+  db.fetchObject(message.author.id + message.guild.id),then(i => {
+  db.fetchObject('userLevel_${message.author.id + message.guild}').then(o => {
+  message.channel.send('Current Blix: '' + (i.value + 1) + ''\nLevel: '' + o.value + ''');
+ }
+  });
+   
 
 client.login(process.env.BOT_TOKEN);
