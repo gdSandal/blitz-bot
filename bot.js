4fae5528a32b0809cafeb10b687e7f17e2303a69
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-03-18 12:43:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,26 +13,6 @@ client.on('message', msg => {
 }});
 
 client.on('message', msg => {
-  if (msg.content === 'b!info') {
-    msg.reply({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-        name: "Made for Geometry Blitz",
-        value: "[Invite Here](https://discord.gg/XSKx36y)."
-      },
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Blitz-Bot"
-    }
-  }
-});
-
-client.on('message', msg => {
   if (msg.content === 'b!embed') {
     msg.reply({embed: {
   color: 3447003,
