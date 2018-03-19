111c415449decb01652a2d10095e927786f9aab0
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-03-18 21:39:11 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,32 +15,17 @@ client.on('message', msg => {
   if (msg.content === 'b!info') {
     msg.reply({embed: {
     color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
+    title: "BlitzBot v0.1.2"
     fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
+        name: "Created by Sandal",
+        value: "Made 3/17/18. Previous Update 3/18/18."
       },
       {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
+        name: "Geometry Blitz",
+        value: "Invite [here](https://discord.gg/XSKx36y)."
       }
     ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Example"
-    }
-  }
+
 });
 
 client.on('message', msg => {
