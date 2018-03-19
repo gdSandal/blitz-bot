9b496e683e7a7cf0261db71bba448c746fb1737d
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-03-18 21:28:12 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,33 @@ client.on('message', msg => {
 
 client.on('message', msg => {
   if (msg.content === 'b!info') {
-    msg.reply('**BlitzBot // v0.0.1** -Made by Sandal-');
+    msg.reply({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
   }
 });
 
