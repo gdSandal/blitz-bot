398d3b6388e2ac5735ba98c46ff40c97bfaf2562
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-22 20:15:22 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,9 +29,72 @@ client.on('message', msg => {
     msg.channel.send({embed: {
   color: 15868795,
   description: "`𝕨𝕖𝕝𝕔𝕠𝕞𝕖 𝕥𝕠 𝕞𝕪 𝕤𝕖𝕣𝕧𝕖𝕣!`"
-    
 }});
   }
 });
 
+client.on('message', msg => {
+  if (msg.content === 's/opline') {
+    msg.channel.send({embed: {
+  color: 15868795,
+  description: "━━━━━━━━━━━━━━━━━━"
+}});
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 's/op3') {
+    msg.channel.send({embed: {
+  color: 15868795,
+  description: "↣Thank you for coming!↢"
+      "Below are the rules and server info"
+      "Please read these before chats/shrines!"
+}});
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 's/op4') {
+    msg.channel.send({embed: {
+  color: 15868795,
+  description: "`𝕣𝕦𝕝𝕖𝕤 𝕠𝕦𝕥𝕡𝕠𝕤𝕥`"
+}});
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 's/op5') {
+    msg.channel.send({embed: {
+  color: 15868795,
+  description: "`𝕨𝕖𝕝𝕔𝕠𝕞𝕖 𝕥𝕠 𝕞𝕪 𝕤𝕖𝕣𝕧𝕖𝕣!`"
+}});
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 's/op6') {
+    msg.channel.send({embed: {
+  color: 15868795,
+  description: "`𝕨𝕖𝕝𝕔𝕠𝕞𝕖 𝕥𝕠 𝕞𝕪 𝕤𝕖𝕣𝕧𝕖𝕣!`"
+}});
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 's/op7') {
+    msg.channel.send({embed: {
+  color: 15868795,
+  description: "`𝕨𝕖𝕝𝕔𝕠𝕞𝕖 𝕥𝕠 𝕞𝕪 𝕤𝕖𝕣𝕧𝕖𝕣!`"
+}});
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 's/op8') {
+    msg.channel.send({embed: {
+  color: 15868795,
+  description: "`𝕨𝕖𝕝𝕔𝕠𝕞𝕖 𝕥𝕠 𝕞𝕪 𝕤𝕖𝕣𝕧𝕖𝕣!`"
+}});
+  }
+});
 client.login(process.env.BOT_TOKEN);
