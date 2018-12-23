77ca9f6fc711de0367cffa6e50d8f2d2ab8e3e8d
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-22 18:30:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('message', msg => {
   if (msg.content === 's!ping') {
-    msg.reply('pong');
+    msg('pong');
   }
 });
 
