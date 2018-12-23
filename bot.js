f000f1c0bd11a21f19399885e329825f9924c6d5
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-22 19:31:29 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("s/");
+
+client.on("ready", () => {
+  console.log(`online`); 
+  client.user.setActivity(`SANCTUM`);
+});
 
 client.on('message', msg => {
-  if (msg.content === '///ready?') {
-    msg.reply('requiem online');
+  if (msg.content === 'ping') {
+    msg.reply('pong');
   }
 });
 
 client.on('message', msg => {
-  if (msg.content === 's!info') {
+  if (msg.content === 'info') {
     msg.reply('`v0.2.0`');
   }
 });
 
 client.on('message', msg => {
-  if (msg.content === 's!img') {
+  if (msg.content === 'img') {
     msg.reply({embed: {
   color: 15868795,
   image: {
