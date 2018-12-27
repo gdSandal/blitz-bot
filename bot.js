1b5bbcbb5f31016098ccd15981474e5918bd5f77
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-26 19:49:44 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,10 +14,10 @@ client.on('message', msg => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 's/text') {
+  if (msg.content === 's/' (message)) {
     msg.channel.send({embed: {
   color: 15868795,
-  description: "z"
+  description: "msg.content"
     
 }});
 }
