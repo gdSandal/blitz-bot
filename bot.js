2b580cb3adcdc089d6de231d4be9796ce3191caa
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-26 23:33:02 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-const args = message.content.slice(prefix.length).split(' ');
-const command = args.shift().toLowerCase();
-
-if(command === "ping") {
-    const m = await message.channel.send("Ping?");
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+    const m = await message.channel.send('Ping?');
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
+    }
+    }});
 
 client.login(process.env.BOT_TOKEN);
