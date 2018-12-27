9b376a29dc10aa08d6425ea7ac016dc2bb972ba4
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-26 23:42:21 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
+  if (msg.content === 's/ping') {
     msg.channel.send({embed: {
-        color: 15868795,
-        description: 'PONG'
-    }});
-    });
+  color: 15868795,
+  description: "PONG"
+    }
+}});
 
 client.login(process.env.BOT_TOKEN);
