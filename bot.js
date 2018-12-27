d4c23aa0b36e1bd89b55d4a9136f864e998faa46
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-26 23:44:52 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,21 @@ const client = new Discord.Client();
 
 client.on('message', msg => {
   if (msg.content === 's/ping') {
+    msg.channel.send('pong');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 's/info') {
+    msg.channel.send('`v0.2.0`');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 's/embed') {
     msg.channel.send({embed: {
   color: 15868795,
-  description: "PONG"
+  description: "this is an embed";
     }
 }});
 
