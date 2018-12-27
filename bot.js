09474b862e16412657e8de50b7bf09c0252decd1
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-26 23:10:57 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', msg => {
-  if (msg.content === 's/ping') {
-    msg.channel.send('pong');
-  }
-});
+const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
 
-client.on('message', msg => {
-  if (msg.content === 's/info') {
-    msg.channel.send('`v0.2.0`');
-  }
-});
 
-  client.on('message', msg => {
-    if (msg.content === 's/say') {
+    if(command === 'say') {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{});
     message.channel.send(sayMessage);
