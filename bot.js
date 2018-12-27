2280aa92ce3d364f67a8c440d8df54181f7287de
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-26 23:38:18 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,10 @@ const client = new Discord.Client();
 
 client.on('message', msg => {
     if (msg.content === 'ping') {
-    const m = await message.channel.send('Ping?');
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    }
+    msg.channel.send({embed: {
+        color: 15868795,
+        description: 'PONG'
     }});
+    });
 
 client.login(process.env.BOT_TOKEN);
