70f8568cacf095ae327b85cfda109a73e1121709
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-26 23:24:00 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
-
 
 if(command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
@@ -11,11 +8,5 @@ if(command === "ping") {
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
-
-if(command === 'say') {
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{});
-    message.channel.send(sayMessage)
-}
 
 client.login(process.env.BOT_TOKEN);
