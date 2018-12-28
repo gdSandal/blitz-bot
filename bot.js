a382270911f8604732735b7c8b4f1df71e2a09a7
gdSandal <37493030+gdSandal@users.noreply.github.com>
2018-12-28 13:31:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+  client.user.setGame('s/help for commands!');
+  console.log(`Requiem initilized.`);
+});
+
 client.on('message', msg => {
   if (msg.content === 's/ping') {
     msg.channel.send({embed: {
