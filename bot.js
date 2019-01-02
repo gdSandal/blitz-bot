5f1619880516f06d5a2624e515ba37e91a325955
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-02 14:56:21 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', msg => {
 
 client.on('message', msg => {
  if (msg.content.startsWith('s/jojowiki')) {
- let args = msg.content.split(" ");
+ let args = msg.content.split(" ").slice(1);
   msg.channel.send('http://jojo.wikia.com/wiki/' + args.join("-"));
  }
 });
