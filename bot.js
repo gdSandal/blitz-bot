66c9355839fa6bda71d257a47404d9c93f3cbc37
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-02 18:31:01 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,15 @@ client.on('message', msg => {
    },
    description: "http://jojo.wikia.com/wiki/" + args.join("_"),
    image: {
-    url: "http://jojo.wikia.com/wiki/List_of_Stands?file=" + args.join() + "AvAnim.png"
+    url: "http://jojo.wikia.com/wiki/List_of_Stands?file=" + args.join("") + "AvAnim.png"
+   },
+   fields: [{
+    name: "Stand name:",
+    value: args.join(" ")
+   },
+   ],
+   footer: {
+    text: "s/standhelp for troubleshooting"
    }
 }});
  }
