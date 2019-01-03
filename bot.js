5df1174a644b62469e45a4d8171a83c63e2b0975
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-02 20:19:47 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', msg => {
 });
 
 client.on('message', msg => {
- if (msg.content.startsWith('s/stand' (' test',' test 2'))) {
+ if (msg.content.startsWith('s/stand ' + 't1','t2')) {
  let args = msg.content.split(" ").slice(1)
   msg.channel.send({embed: {
    color: 15868795,
