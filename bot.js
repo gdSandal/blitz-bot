1089f11543d18397bb673fafdc51ae8efba68bef
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-07 16:31:02 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('message', msg => {
 client.on('message', msg => {
  if (msg.content === 's/edit') {
   msg.channel.send("hello")
-  .then(msg.channel.send("hello")3000);
+  .then(msg.channel.send("hello"(3000));
  }
 });
 
