f9afd65c05d9694f7bdd64d9466ca44365213f66
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-09 14:08:28 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -126,11 +126,14 @@ client.on('message', msg => {
 });
 
 client.on('message', msg => {
- if (msg.content.startsWith('s/stand ' + 'Star Platinum' || 'Magician’s Red' || 'Hermit Purple' || 'Hierophant Green','Silver Chariot','The Fool','Tower of Grey','Dark Blue Moon','Strength','Ebony Devil','Yellow Temperance','Hanged Man','Emperor','Empress','Wheel of Fortune','Justice','Lovers','TheSun','Death 13','Judgement','High Priestess','Geb','Khnum','Thoth','Anubis','Bast','Sethan','Osiris','Horus','Atum','Tenor Sax','Cream','The World','Crazy Diamond','The Hand','Echoes','Heaven’s Door','Aqua Necklace','Bad Company','Red Hot Chili Pepper','The Lock','Surface','Love Deluxe','Pearl Jam','Achtung Baby','Ratt','Harvest','Cinderella','Boy II Man','Earth, Wind and Fire','Highway Star','Super Fly','Eigma','Cheap Trick','Stray Cat','Atom Heart Father','Killer Queen')) {
+ if (msg.content.startsWith('s/stand')) {
+ const filter = ['Star Platinum', 'Magician’s Red', 'Hermit Purple', 'Hierophant Green','Silver Chariot','The Fool','Tower of Grey','Dark Blue Moon','Strength','Ebony Devil','Yellow Temperance','Hanged Man','Emperor','Empress','Wheel of Fortune','Justice','Lovers','TheSun','Death 13','Judgement','High Priestess','Geb','Khnum','Thoth','Anubis','Bast','Sethan','Osiris','Horus','Atum','Tenor Sax','Cream','The World','Crazy Diamond','The Hand','Echoes','Heaven’s Door','Aqua Necklace','Bad Company','Red Hot Chili Pepper','The Lock','Surface','Love Deluxe','Pearl Jam','Achtung Baby','Ratt','Harvest','Cinderella','Boy II Man','Earth, Wind and Fire','Highway Star','Super Fly','Eigma','Cheap Trick','Stray Cat','Atom Heart Father','Killer Queen'];
  let args = msg.content.split(" ").slice(1)
  let wikilink = "http://jojo.wikia.com/wiki/List_of_Stands?file=" + args.join("") + "AvAnim.png";
  let googlelink = "https://www.google.com/search?q=" + args.join("+") + "&client=safari&hl=en";
-  msg.channel.send({embed: {
+  msg.channel.awaitMessages(filter, {max: 1, time: 60000})
+ .then(collected => {
+ msg.channel.send({embed: {
    color: 15868795,
    title: "__| JJBA Stand Stats |__",
    thumbnail: {
@@ -152,6 +155,7 @@ client.on('message', msg => {
    footer: {
     text: "type - s/standhelp for troubleshooting"
    }
+             }
 }});
  }
 });
@@ -296,8 +300,8 @@ client.on('message', msg => {
   }
 });
 
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find(ch => ch.name === 'sanctum');
+(client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === args);
   if (!channel) return;
   channel.send(`Welcome to the server! ${member}`);
 });
