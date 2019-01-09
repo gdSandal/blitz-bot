35fcae61e1ed4b42218067cef5864ac73c56ceb3
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-08 19:39:23 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ msg.edit('```_________\n|_______|/\n|' + s[sI] + x[xI] + y[yI] + '|/   RESULT:  
 client.on('message', msg => {
  if (msg.content === 's/test2') {
 const filter = (reaction, user) => {
-    return ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '⏸',].includes(reaction.emoji.name) && user.id === msg.author.id;
+    return ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '⏸'].includes(reaction.emoji.name) && user.id === msg.author.id;
 }
   msg.channel.send({embed: {
    color: 15868795,
@@ -58,7 +58,7 @@ const filter = (reaction, user) => {
     text: 'React to navigate pages [1/4]'
    }
   }}).then(msg => {
-  msg.react('1️⃣').then(() => msg.react('2️⃣').then(() => msg.react('3️⃣').then(() => msg.react('4️⃣').then(() => msg.react('⏸')))));
+  msg.react('1️⃣').then(() => msg.react('2️⃣')).then(() => msg.react('3️⃣')).then(() => msg.react('4️⃣')).then(() => msg.react('⏸'));
 msg.awaitReactions(filter, { max: 1, time: 60000 })
     .then(collected => {
         const reaction = collected.first();
