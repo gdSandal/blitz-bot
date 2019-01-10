8b57f39d12462e3611f091d104283e7ac28a0c23
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-09 19:22:10 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,6 +65,7 @@ msg.awaitReactions(filter, { max: 3, time: 60000 })
         const reaction = collected.first();
         if (reaction.emoji.name === '⏩') {
          reaction.remove(reaction.users.last());
+         reaction.remove('⏸');
             msg.edit({embed: {
    color: 15868795,
    title: 's/help - PAGE (3)',
@@ -119,7 +120,6 @@ msg.awaitReactions(filter, { max: 3, time: 60000 })
         const reactiont = collected.first();
     if (reactiont.emoji.name === '⏩') {
      reactiont.remove(reactiont.users.last());
-     reactiont.remove('⏸');
             msg.edit({embed: {
    color: 15868795,
    title: 's/help - PAGE (1)',
