f50969bc880f2d99d3195fc2e910243b45ba7ecd
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-11 18:10:51 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,8 +55,7 @@ client.on('message', msg => {
   .then(msg => {
   msg.channel.awaitMessages(filter, { max: 1, time: 60000 })
   .then(collected => {
-   const x = collected.first();
-   const r = x.shift().toLowerCase();
+   const r = collected.first().toLowerCase();
    if (r.content === "red") {
     if (r.content === rb[rbi]) {
      msg.channel.send("**You bet on: ** 🔴 " + r.content + "\n**Roulette Result:** " + rb[rbi] + " " + nb[nbi] + " \n**WIN!** 🎉");
