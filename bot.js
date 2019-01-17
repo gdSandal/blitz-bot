8d3a0d3508f2424c22499d754f0763fb3f2dafea
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-17 16:50:26 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('message', msg => {
  if (msg.content.startsWith("s/theworld")) {
   if (msg.content === "s/theworld") {
    msg.channel.send("Please name a channel");
-  }else {
+  }else
   if (!msg.member.hasPermission("MANAGE PERMISSIONS")) return;
   let args = msg.content.split(" ").slice(1);
   let chn = args.join("")
