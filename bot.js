33c5080d96426f9afe3609c043a190b50af5af84
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-17 10:14:48 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', msg => {
   title: "Channel temporarily locked",
   description: "[ZA~WARUDO](https://youtu.be/-FT23AwNPOM) | Time stopped",
   image: {
-   url: "https://cdn.discordapp.com/attachments/470359851227414532/535490838327853088/image0.gif"
+   url: "https://cdn.discordapp.com/attachments/470359851227414532/535492022748839936/image0.gif"
   },
   footer: {
    text: "Channel freeze lasts for 5 seconds"
