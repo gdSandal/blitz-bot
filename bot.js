3519c311bc616443c3a7b6df3ed4a9b920f951db
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-23 09:13:16 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,8 @@ client.on('ready', () => {
 client.on('message', msg => {
  if (msg.content.startsWith("s/moodyblues")) {
  let u = msg.mentions.members.first();
+ let args = msg.content.split(" ").slice(1);
+ if args.length(0) return msg.channel.send("error");
   msg.channel.send(u.id);
  }});
 
