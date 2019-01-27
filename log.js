701afc033a8ad242645a6d7b7659fb5d5df9c150
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-26 18:41:25 -0600

    Update log.js

--- log.js
+++ log.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', msg => {
-if (msg.content === "s/log") {
+if (msg.content.startsWith("s/log")) {
 msg.channel.send("yes. log.");
 }});
 
