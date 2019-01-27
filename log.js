257c5e1e691b384dd3a36b0a06a009fca1b65667
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-26 18:44:10 -0600

    Delete log.js

--- log.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('message', msg => {
-if (msg.content.startsWith("s/log")) {
-msg.channel.send("yes. log.");
-}});
-
-client.login(process.env.BOT_TOKEN);
