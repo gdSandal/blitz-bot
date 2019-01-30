9bf74280aa997dc88f45e3a505da636609551136
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-30 16:23:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ client.on('message', msg => {
   msg.channel.send({ embed: {
    color: 15868795,
    title: "**Welcome to Sanctum!**",
-   description: 1+2+2+2+2+2+2+2+2+2+3 + "\n" + " Thanks for joining!"
+   description: 1 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 3 + "\n" + " Thanks for joining!"
    }});
  }});
 
