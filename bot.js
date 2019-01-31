591027609d9f21f286945d840f61af4313a3cde3
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-01-30 19:30:53 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('message', msg => {
   if (!rmember) return;
   msg.channel.send({ embed: {
    color: 15868795,
-   title: rmember.tag + " has achieved the rank of **Capo!**",
+   title: rmember.user.username + " has achieved the rank of **Capo!**",
    description: "Congratulations " + rmember + "!"
   }});
   rmember.addRole(role);
