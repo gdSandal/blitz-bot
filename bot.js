b2a09d789625b9caa7cfcb397740a2447043e0b1
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-02-08 18:58:57 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
-const coins = require("./xp.json");
 
 client.on('ready', () => {
  client.user.setPresence({
@@ -778,39 +776,6 @@ client.on('message', msg => {
 });
 	
 	
-	client.on("message", message => {
-	
-	if(!coins[message.author.id]){
-	  coins[message.author.id] = {
-	    coins: 0
-	  }
-	}
-	
-	let coinAmt = 1;
-	let baseAmt = 1;
-	console.log(coinAmt , baseAmt);
-	
-	if(coinAmt = baseAmt){
-	  coins[message.author.id] = {
-	    coins: coins[message.author.id].coins + coinAmt
-	  };
-	  fs.writeFile("./xp.json", JSON.stringify (coins), (err) => {
-	    if (err) console.log(err);
-	  });
-	}
-	});
-	
-	client.on("message", msg => {
-	
-	      let uCoins = coins[msg.author.id].coins;
-	
-	      if(msg.content === "s/credits") {
-	          msg.channel.send({ embed: {
-	            color: 15868795,
-	            title: msg.author.username + ", you have:",
-	            description: uCoins + "¥ credits"
-	          }});
-	      }
-	  });
+
 
 client.login(process.env.BOT_TOKEN);
