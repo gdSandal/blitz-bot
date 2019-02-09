eff883ac033afa15ed41846ed01fe6ae45995548
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-02-09 17:55:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const mongoose = require('mongoose');
+const db = require('quick.db');
 
 client.on('ready', () => {
  client.user.setPresence({
