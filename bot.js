8f4d933382fbda4eec4c5ec15d99af28c726174d
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-02-10 15:00:56 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -856,7 +856,7 @@ client.on("message", msg => {
 		let amt = msg.content.split(" ").slice(2);
 		if(!u) return;
 		if(!amt) return;
-		msg.channel.send(u.user.username + amt);
+		msg.channel.send(msg.author.username + " gifted " + u.user.username + " " + amt + "¥ credits!");
 	}});
 	
 
