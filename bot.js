bde7b60d279f4973013720474f3971719c6ae092
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-02-10 12:12:29 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const pet = require('./pets.json');
 
 client.on('ready', () => {
  client.user.setPresence({
