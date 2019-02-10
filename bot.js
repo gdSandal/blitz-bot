18f75677d7107769c14aa7b162ad302c1b80b6fa
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-02-10 12:28:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -786,6 +786,40 @@ client.on('message', msg => {
   }
 });
 	
+client.on("message", message => {
+	
+	if(!coins[message.author.id]){
+	  coins[message.author.id] = {
+	    coins: 0
+	  }
+	}
+	
+	let coinAmt = 1;
+	let baseAmt = 1;
+	console.log(coinAmt , baseAmt);
+	
+	if(coinAmt === baseAmt){
+	  coins[message.author.id] = {
+	    coins: coins[message.author.id].coins + coinAmt
+	  };
+	  fs.writeFile("./xp.json", JSON.stringify (coins), (err) => {
+	    if (err) console.log(err);
+	  });
+	}
+	});
+	
+	client.on("message", msg => {
+	
+	      let uCoins = coins[msg.author.id].coins;
+	
+	      if(msg.content === "s/credits") {
+	          msg.channel.send({ embed: {
+	            color: 15868795,
+	            title: msg.author.username + ", you have:",
+	            description: uCoins + "¥ credits"
+	          }});
+	      }
+	  });
 	
 
 
