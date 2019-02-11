a99d8d757cd7d2381f24bdddd66416dfcf546881
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-02-11 16:53:58 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ client.on("message", msg => {
 	    coins: coins[u.id].coins - 100
 	  };
 		pet[u.id] = {
-	    pet: pet[u.id].(p[pi])
+	    pet: p[pi].toString
 	  };
 			
 	  fs.writeFile("./xp.json", JSON.stringify (coins), (err) => {
