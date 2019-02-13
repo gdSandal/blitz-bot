29777bacee750ad6faaba141db51940c1943135f
gdSandal <37493030+gdSandal@users.noreply.github.com>
2019-02-13 15:57:35 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -114,37 +114,37 @@ client.on("message", msg => {
 	  }});
 	  } else{
 	  if(uPet === "Lab "+a){
-		  var y = "https://cdn.discordapp.com/attachments/470359851227414532/544678783081250816/image0.gif";
+		  var y = "http://i.imgur.com/HQQSKSO.jpg";
 	  }
 	    if(uPet === "Wolf "+a){
-		  var y = "https://cdn.discordapp.com/attachments/470359851227414532/544678807936827392/image0.gif";
+		  var y = "http://i.imgur.com/7zHH56f.jpg";
 	  }
 	    if(uPet === "Brown Cat "+a){
-		  var y = "https://cdn.discordapp.com/attachments/470359851227414532/544678835182895107/image0.gif";
+		  var y = "http://i.imgur.com/GcoIPyd.jpg";
 	  }
 	    if(uPet === "White Cat "+a){
-		  var y = "https://cdn.discordapp.com/attachments/470359851227414532/544678857706438674/image0.gif";
+		  var y = "http://i.imgur.com/sEfQDDO.jpg";
 	  }
 	    if(uPet === "Striped Cat "+a){
-		  var y = "https://cdn.discordapp.com/attachments/470359851227414532/544678879500042251/image0.gif";
+		  var y = "http://i.imgur.com/oFXZLPC.jpg";
 	  }
 	    if(uPet === "Monkey "+a){
-		  var y = "https://cdn.discordapp.com/attachments/470359851227414532/544679134639685632/image0.gif";
+		  var y = "http://i.imgur.com/l5E0jfF.jpg";
 	  }
 	    if(uPet === "Turtle "+a){
-		  var y = "https://cdn.discordapp.com/attachments/470359851227414532/544679173017567262/image0.gif";
+		  var y = "http://i.imgur.com/d361ijy.jpg";
 	  }
 	    if(uPet === "Bunny "+a){
-		  var y = "https://cdn.discordapp.com/attachments/470359851227414532/544679151861235712/image0.gif";
+		  var y = "http://i.imgur.com/aMudQ66.jpg";
 	  }
 	    if(uPet === "Blue Fish "+a){
-		  var y = "https://cdn.discordapp.com/attachments/470359851227414532/544679071573868546/image0.gif";
+		  var y = "http://i.imgur.com/sIE9WJS.jpg";
 	  }
 	    if(uPet === "Red Fish "+a){
-		  var y = "https://cdn.discordapp.com/attachments/470359851227414532/544679052875792407/image0.gif";
+		  var y = "http://i.imgur.com/caA0A7L.jpg";
 	  }
 	    if(uPet === "Hedgehog "+a){
-		    var y = "https://cdn.discordapp.com/attachments/470359851227414532/544980897917566986/image0.gif";
+		    var y = "http://i.imgur.com/9kQqCWb.jpg";
 	  }
 		  
 		  //levels
